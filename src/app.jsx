2f8ae6f283552a87b8dda165093b82f8a087043b
Rustam Kudratov <kudratov.rustik@gmail.com>
2025-03-20 23:18:14 +0300

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Div = styled.div`
+	text-align: center;
+`
+
+export const App = () => {
+	return (
+		<Div>
+			<div>Blog</div>
+			<i className="fa-solid fa-circle-user"></i>
+		</Div>
+	)
+}
