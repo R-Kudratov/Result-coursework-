22feaf6305fcafcbf54413e34accaae3f923b8dd
Rustam Kudratov <kudratov.rustik@gmail.com>
2025-03-28 01:00:42 +0300

    Реализовать основной компонент блога с роутингом

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Blog } from './blog'
 import './index.css'
-import { App } from './app'
 
-createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+	<BrowserRouter>
+		<Blog />
+	</BrowserRouter>,
 )
